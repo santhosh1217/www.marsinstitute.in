a2d4a4418ffb8ed1c6be053bac452e1c20e7e970
Man-o <manoayyappan2503@gmail.com>
2024-02-15 20:45:28 +0530

    commited

--- index.js
+++ index.js
@@ -15,3 +15,4 @@ app.use("/api",apiRoute)
 app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")))
 
 app.listen(3000,()=>console.log("server started successfully"))
+//comment
