d14321c00340a15df13f174592bdebea8484e352
Indian-1234 <indiangithub321@gmail.com>
2024-03-03 12:26:36 +0530

    change html field in stateborad or cbsce

--- services/apiService.js
+++ services/apiService.js
@@ -4,7 +4,7 @@ const { sendmailHr,sendmailStudent} = require("./mailService")
 function register(req, res) 
 {
     console.log(req.body)
-    const data = { name: req.body.name, mail: req.body.mail, mobile: req.body.mobile,course:req.body.course, studymode: req.body.studymode,standard:req.body.standard, duration :req.body.duration }
+    const data = { name: req.body.name,boardtype:req.body.board, mail: req.body.mail, mobile: req.body.mobile,course:req.body.course, studymode: req.body.studymode,standard:req.body.standard, duration :req.body.duration }
     sendmailHr(data)
     sendmailStudent(data)
     insert(data)
